50087b76dde93db1ff45e31ea32faf53f58535a4
ArtemidaOctavia <42804865+ArtemidaOctavia@users.noreply.github.com>
2020-05-25 16:05:21 +0300

    Updates

---
+++ precache-manifest.9ca34b6b386c34c63dde49af1ad6820d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ab17042a231a133b7ecfff7935019c",
+    "url": "/dice-thrower/index.html"
+  },
+  {
+    "revision": "38c62ceb15b2781e4574",
+    "url": "/dice-thrower/static/css/main.1bcadc11.chunk.css"
+  },
+  {
+    "revision": "db1c122c0a6190db4a62",
+    "url": "/dice-thrower/static/js/2.44d3b880.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dice-thrower/static/js/2.44d3b880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c62ceb15b2781e4574",
+    "url": "/dice-thrower/static/js/main.547ee06e.chunk.js"
+  },
+  {
+    "revision": "dfac911ad5057a8980cf",
+    "url": "/dice-thrower/static/js/runtime-main.6401d24e.js"
+  }
+]);
